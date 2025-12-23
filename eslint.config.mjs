267109be8064c121e0a17786b7568a59e615f303
fronglemog <26048422+fronglemog@users.noreply.github.com>
2025-12-23 19:59:19 +1100

    eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -23,7 +23,7 @@ export default defineConfig([
     plugins: {
       importPlugin,
       js,
-      tseslint
+      '@typescript-eslint' : tseslint
     },
     extends: [
       "js/recommended",
