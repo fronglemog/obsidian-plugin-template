457aedfa108a9f41d2fd9e31234046b38129c41d
fronglemog <26048422+fronglemog@users.noreply.github.com>
2025-12-23 20:54:01 +1100

    eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,23 @@
 // eslint.config.mjs
 
-import js from "@eslint/js";
-import tseslint from "typescript-eslint";
-import { defineConfig, globalIgnores } from "eslint/config";
-import importPlugin from "eslint-plugin-import";
+/* ESLINT */
 import globals from "globals";
+import { defineConfig, globalIgnores } from "eslint/config";
+import js from "@eslint/js";
 
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import tsParser from "@typescript-eslint/parser";
+/* PLUGINS */
+import tseslint from "typescript-eslint";
+import obsidianmd from 'eslint-plugin-obsidianmd';
+import importPlugin from "eslint-plugin-import";
 
 export default defineConfig([
   js.configs.recommended,
   tseslint.configs.recommended,
+  obsidianmd.
 
   globalIgnores([
     "**/node_modules/",
+    "eslint.config.mjs",
     "**/main.js"
   ]),
 
@@ -23,29 +26,30 @@ export default defineConfig([
     plugins: {
       importPlugin,
       js,
-      '@typescript-eslint' : tseslint
+      '@typescript-eslint' : tseslint.plugin
     },
     extends: [
       "js/recommended",
-      "plugin:@typescript-eslint/eslint-recommended",
-      "plugin:@typescript-eslint/recommended"
+      "@typescript-eslint/eslint-recommended",
+      "@typescript-eslint/recommended"
     ],
     languageOptions: {
       globals: {
         ...globals.node,
         ...globals.browser
       },
-      parser: tsParser,
       ecmaVersion: "latest",
       sourceType: "module",
     },
 
     rules: {
+      // ESLint
       "no-undef" : "warn",
       "no-unused-vars": "off",
       "no-var" : "error",
       "no-prototype-builtins": "off",
 
+      // TypeScript ESLint
       "@typescript-eslint/no-unused-vars": "warn",
       "@typescript-eslint/no-empty-function": "off",
       "@typescript-eslint/ban-ts-comment": "off",
