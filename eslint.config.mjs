ce34b9f870850c8b1f4903287f4c04b87e3a9756
fronglemog <26048422+fronglemog@users.noreply.github.com>
2026-08-29 21:17:46 +1000

    Update project config files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,4 @@
-// eslint.config.mjs
-
+/* ========================= IMPORTS ========================= */
 /* ESLINT */
 import { defineConfig, globalIgnores } from 'eslint/config';
 import js from '@eslint/js';
@@ -11,21 +10,23 @@ import eslintPluginUnicorn from 'eslint-plugin-unicorn';
 import eslintPluginSvelte from 'eslint-plugin-svelte';
 import eslintPluginObsidianmd from 'eslint-plugin-obsidianmd';
 
+/* ========================= ESLINT CONFIG ========================= */
 export default defineConfig([
   globalIgnores([
     '**/node_modules/',
     '**/build/',
     '_local/**',
     'plugin/**',
-    'eslint.config.mjs',
-    'esbuild.config.mjs',
-    'version-bump.mjs'
+    // 'eslint.config.mjs',
+    // 'esbuild.config.mjs',
+    // 'version-bump.mjs'
   ]),
 
-  ...eslintPluginObsidianmd.configs.recommended,
-
   {
-    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    files: [
+      '**/*.{js,mjs,cjs,ts,mts,cts}',
+      '**/*.{svelte,svelte.js,svelte.ts}'
+    ],
     plugins: {
       js,
       '@typescript-eslint': tseslint.plugin,
@@ -33,10 +34,12 @@ export default defineConfig([
       obsidianmd: eslintPluginObsidianmd
     },
     extends: [
-      'js/recommended',
-      '@typescript-eslint/eslint-recommended',
-      '@typescript-eslint/recommended',
-      // 'unicorn/recommended',
+      js.configs.recommended,
+      tseslint.configs.eslintRecommended,
+      tseslint.configs.recommended,
+      tseslint.configs.recommendedTypeCheckedOnly,
+      // eslintPluginUnicorn.configs.recommended,
+      eslintPluginObsidianmd.configs.recommended
     ],
     languageOptions: {
       globals: {
@@ -53,10 +56,13 @@ export default defineConfig([
     },
     rules: {
       // ESLint
-      'no-prototype-builtins' : 'off',
-      'no-undef' : 'off',
+      'no-empty': 'warn',
+      'no-prototype-builtins': 'off',
+      'no-undef': 'off',
       'no-unused-vars': 'off',
-      'no-var' : 'error',
+      'no-var': 'error',
+      'eslint-comments/require-description': 'off',
+      'no-throw-literal': 'off',
 
       // TypeScript ESLint
       '@typescript-eslint/ban-ts-comment' : 'off',
@@ -69,13 +75,15 @@ export default defineConfig([
       '@typescript-eslint/no-misused-promises' : 'off',
       '@typescript-eslint/no-namespace' : 'warn',
       '@typescript-eslint/no-redundant-type-constituents' : 'off',
+      '@typescript-eslint/no-unnecessary-type-assertion': 'warn',
       '@typescript-eslint/no-unsafe-assignment' : 'off',
       '@typescript-eslint/no-unsafe-member-access': 'warn',
       '@typescript-eslint/no-unused-vars' : 'warn',
+      '@typescript-eslint/only-throw-error': 'error',
 
       // eslint-plugin-unicorn
       'unicorn/no-array-callback-reference': 'error',
-      'unicorn/no-array-for-each': 'error',
+      'unicorn/no-for-each': 'error',
       'unicorn/no-array-reduce': 'warn',
       'unicorn/no-array-reverse': 'warn',
       'unicorn/no-array-sort': 'warn',
@@ -87,7 +95,7 @@ export default defineConfig([
       'unicorn/no-negation-in-equality-check': 'warn',
       'unicorn/no-static-only-class': 'warn',
       'unicorn/no-unnecessary-array-flat-depth': 'warn',
-      'unicorn/prefer-global-this': 'warn',
+      'unicorn/prefer-global-this': 'off',
       'unicorn/prefer-modern-dom-apis': 'warn',
       'unicorn/prefer-node-protocol': 'warn',
       'unicorn/prefer-object-from-entries': 'warn',
@@ -96,32 +104,23 @@ export default defineConfig([
       'obsidianmd/no-static-styles-assignment' : 'warn',
       'obsidianmd/ui/sentence-case' : 'off',
       'obsidianmd/validate-manifest' : 'off',
+      'obsidianmd/no-global-this': 'off',
+      'obsidianmd/no-nodejs-modules': 'off',
     },
   },
 
+  /* SVELTE */
   {
-    files: ['**/*.svelte', '**/*.svelte.ts', '**/*.svelte.js'],
+    files: [
+      '**/*.{svelte,svelte.js,svelte.ts}'
+    ],
     plugins: {
-      js,
-      '@typescript-eslint': tseslint.plugin,
-      unicorn: eslintPluginUnicorn,
-      obsidianmd: eslintPluginObsidianmd,
       svelte: eslintPluginSvelte,
     },
     extends: [
-      '@typescript-eslint/eslint-recommended',
-      '@typescript-eslint/recommended',
-      // 'unicorn/recommended',
-      'svelte/recommended'
+      eslintPluginSvelte.configs.recommended
     ],
     languageOptions: {
-      globals: {
-        ...globals.builtin,
-        ...globals.node,
-        ...globals.browser
-      },
-      ecmaVersion: 'latest',
-      sourceType: 'module',
       parserOptions: {
         projectService: true,
         extraFileExtensions: ['.svelte'],
@@ -129,51 +128,6 @@ export default defineConfig([
       }
     },
     rules: {
-      // ESLint
-      'no-prototype-builtins' : 'off',
-      'no-undef' : 'off',
-      'no-unused-vars': 'off',
-      'no-var' : 'error',
-
-      // TypeScript ESLint
-      '@typescript-eslint/ban-ts-comment' : 'off',
-      '@typescript-eslint/consistent-type-exports': 'error',
-      '@typescript-eslint/consistent-type-imports': 'error',
-      '@typescript-eslint/no-deprecated': 'warn',
-      '@typescript-eslint/no-empty-function' : 'off',
-      '@typescript-eslint/no-empty-object-type' : 'off',
-      '@typescript-eslint/no-explicit-any' : 'off',
-      '@typescript-eslint/no-misused-promises' : 'off',
-      '@typescript-eslint/no-namespace' : 'warn',
-      '@typescript-eslint/no-redundant-type-constituents' : 'off',
-      '@typescript-eslint/no-unsafe-assignment' : 'off',
-      '@typescript-eslint/no-unsafe-member-access': 'warn',
-      '@typescript-eslint/no-unused-vars' : 'warn',
-
-      // eslint-plugin-unicorn
-      'unicorn/no-array-callback-reference': 'error',
-      'unicorn/no-array-for-each': 'error',
-      'unicorn/no-array-reduce': 'warn',
-      'unicorn/no-array-reverse': 'warn',
-      'unicorn/no-array-sort': 'warn',
-      'unicorn/no-for-loop': 'warn',
-      'unicorn/no-immediate-mutation': 'warn',
-      'unicorn/no-instanceof-builtins': 'warn',
-      'unicorn/no-magic-array-flat-depth': 'warn',
-      'unicorn/no-negated-condition': 'warn',
-      'unicorn/no-negation-in-equality-check': 'warn',
-      'unicorn/no-static-only-class': 'warn',
-      'unicorn/no-unnecessary-array-flat-depth': 'warn',
-      'unicorn/prefer-global-this': 'warn',
-      'unicorn/prefer-modern-dom-apis': 'warn',
-      'unicorn/prefer-node-protocol': 'warn',
-      'unicorn/prefer-object-from-entries': 'warn',
-
-      // eslint-plugin-obsidianmd
-      'obsidianmd/no-static-styles-assignment' : 'warn',
-      'obsidianmd/ui/sentence-case' : 'off',
-      'obsidianmd/validate-manifest' : 'off',
-
       // eslint-plugin-svelte
 
     }
